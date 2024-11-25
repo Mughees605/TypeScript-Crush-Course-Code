917167fff695a0aaed619f5487d423c1caaa325c
mughees08 <muhammadmughees08@gmail.com>
2024-11-25 19:16:49 +0500

    Remove extra consoles

--- function_rest_parameter/app.js
+++ function_rest_parameter/app.js
@@ -6,7 +6,6 @@ var employeeName = buildName("Moheed", "Shoaib", "Dawood", "Wahaj");
 console.log(`Employee Names: ${employeeName}`);
 //anonymous function type with Rest parameters
 var buildNameFun = function (firstName, ...restOfName) {
-    return firstName + "," + restOfName.join(",");
+    return firstName + " " + restOfName.join(" ");
 };
 var employeeName = buildNameFun("Noman", "Tahir", "Kashaf", "Rehman");
-console.log(`Employee Names: ${employeeName}`);
--- function_rest_parameter/app.ts
+++ function_rest_parameter/app.ts
@@ -9,9 +9,7 @@ console.log(`Employee Names: ${employeeName}`)
 //anonymous function type with Rest parameters
 var buildNameFun: (fname: string, ...rest: string[])=>string =
 function (firstName: string, ...restOfName: string[]) {
-	return firstName + "," + restOfName.join(",");
+	return firstName + " " + restOfName.join(" ");
 }
 
 var employeeName = buildNameFun("Noman", "Tahir", "Kashaf", "Rehman");
-
-console.log(`Employee Names: ${employeeName}`)
