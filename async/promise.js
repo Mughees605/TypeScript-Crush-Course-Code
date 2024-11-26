e56d0d17f685e5eb851a5b9a9ce0473efa62c9b7
Moheed789 <moheedsiddiqui74@gmail.com>
2024-11-26 19:00:54 +0500

    TypeScript Playlist 3rd video completed in this video Asynchronous Programming

---
+++ async/promise.js
@@ -0,0 +1,19 @@
+"use strict";
+function delay(milliseconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, milliseconds);
+    });
+}
+//Using `then`, `catch`, and `finally`:
+delay(1000)
+    .then(() => {
+    console.log('The delay is over');
+})
+    .catch((error) => {
+    console.error('Something went wrong:', error);
+})
+    .finally(() => {
+    console.log('This is always executed');
+});
